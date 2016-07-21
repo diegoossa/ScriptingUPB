2c61cdc8152feb9177623bf14a02e07f3e270cae
diegoossa <diegoossa@gmail.com>
2016-07-21 08:23:03 -0500

    Initial Commit
    Ejemplo de GetComponent simple, haciendo play y pause a un AudioSource.

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    /// <summary>
+    /// Inicializacion
+    /// </summary>
+    void Start()
+    {
+        //Se obtiene el componente AudioSource de este GameObject y se guarda en la variable _audioSource
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Se usa para reproducir el sonido contenido en el AudioSource
+    /// </summary>
+    public void PlayAudio()
+    {
+        //Se usa el AudioSource y se usa su funcion Play
+        _audioSource.Play();
+    }
+
+    /// <summary>
+    /// Se usa para pausar el sonido contenido en el AudioSource
+    /// </summary>
+    public void PauseAudio()
+    {
+        //Se usa el AudioSource y se usa su funcion Play
+        _audioSource.Pause();
+    }
+
+}
